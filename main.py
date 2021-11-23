9fb416c136ac9b0a13fa000528a8cc2dd7132e65
zlatkoos <zlatko.jurkovic@hotmail.com>
2021-11-23 11:17:39 +0100

    Domaca zadaca 15.2 dovrseno.

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about_me():
+    return render_template("about.html")
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+if __name__ == "__main__":
+    app.run(use_reloader=True)
